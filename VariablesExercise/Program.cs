51889a489c69e5150217a1afa6b1cd5caa00bd55
[Joseph Gaughan] <[jojopotato9393@gmail.com]>
2024-01-11 11:33:48 -0600

    fix names

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -11,19 +11,19 @@
             int myAge = 17;
             char initial = 'J';
             bool isCodingFun = true;
-            double Time = 10.21;
-            decimal Compleated = 0.09m;
+            double time = 10.21;
+            decimal compleated = 0.09m;
 
 
             Console.WriteLine($"My Name is {myName}, I am {myAge} years old.");
 
-            Console.WriteLine($"you can call me {initial}, its {Time}AM for me. I am {Compleated} done with TrueCoders.");
+            Console.WriteLine($"you can call me {initial}, its {time}AM for me. I am {compleated} done with TrueCoders.");
 
             Console.WriteLine($"I think Coding is great {isCodingFun}.");
 
             Console.WriteLine("How long have you been with true Coders?");
            string response  = Console.ReadLine();
-            Console.WriteLine($" {response} wow thats a long time.");
+            Console.WriteLine($"{response} wow thats a long time.");
         }
     }
 }
