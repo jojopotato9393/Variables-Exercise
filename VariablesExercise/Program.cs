5b404e73e94368c5130727b0f278abc202aaf242
[Joseph Gaughan] <[jojopotato9393@gmail.com]>
2024-01-11 10:56:26 -0600

    Variables are Fun

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // declare
+            string myName;
+            //initialize
+            myName = "Joseph";
+            int myAge = 17;
+            char initial = 'J';
+            bool isCodingFun = true;
+            double Time = 10.21;
+            decimal Compleated = 0.09m;
+
+
+            Console.WriteLine($"My Name is {myName}, I am {myAge} years old.");
+
+            Console.WriteLine($"you can call me {initial}, its {Time}AM for me. I am {Compleated} done with TrueCoders.");
+
+            Console.WriteLine($"I think Coding is great {isCodingFun}.");
+
+            Console.WriteLine("How long have you been with true Coders?");
+           string response  = Console.ReadLine();
+            Console.WriteLine($" {response} wow thats a long time.");
         }
     }
 }
